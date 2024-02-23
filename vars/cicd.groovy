477872e2d4a74315f605610051d3136d4680c586
Gopikrishna970 <158047016+Gopikrishna970@users.noreply.github.com>
2024-02-23 12:48:38 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,14 @@
+def newdownload(repo)
+{
+  git "https://github.com/intelliqittrainings/${repo}"
+}
+def newbuild()
+{
+  sh 'mvn package'
+}
+def deploy(jobname,ip,appname)
+{
+  sh "scp/var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war  ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"}
+def testing(jobname)
+{
+   sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'}
